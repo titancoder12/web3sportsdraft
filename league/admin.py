e0960c9c61ea6f755b1949fb4dc283835b0d0830
Eugene <eugene@Eugenes-iMac.local>
2025-03-07 01:28:25 -0800

    divisions: admin

--- league/admin.py
+++ league/admin.py
@@ -1,14 +1,25 @@
 # league/admin.py
 from django.contrib import admin
-from .models import Team, Player, DraftPick
+from .models import League, Division, Team, Player, DraftPick
 
 admin.site.site_title = "Baseball League Admin"
 admin.site.site_header = "Baseball League Admin"
 admin.site.index_title = "Welcome to Baseball League Admin"
 
+@admin.register(League)
+class LeagueAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Division)
+class DivisionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'league')
+    list_filter = ('league',)
+
 @admin.register(Team)
 class TeamAdmin(admin.ModelAdmin):
+    list_display = ('name', 'division')
     filter_horizontal = ('coaches',)
+    list_filter = ('division',)
 
 @admin.register(Player)
 class PlayerAdmin(admin.ModelAdmin):
@@ -16,4 +27,7 @@ class PlayerAdmin(admin.ModelAdmin):
     list_filter = ('team',)
     search_fields = ('name', 'description', 'coach_comments')
 
-admin.site.register(DraftPick)
+@admin.register(DraftPick)
+class DraftPickAdmin(admin.ModelAdmin):
+    list_display = ('division', 'team', 'player', 'pick_number', 'round_number')
+    list_filter = ('division',)
