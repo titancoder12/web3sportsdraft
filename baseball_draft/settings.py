528d091dc2affdddde6c6407dc638b24ae47d394
Eugene <eugene@Eugenes-iMac.local>
2025-03-08 23:58:04 -0800

    login esthetics

--- baseball_draft/settings.py
+++ baseball_draft/settings.py
@@ -119,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/home/django-user/web3sportsdraft/static'
+STATICFILES_DIRS = [
+    BASE_DIR / "league/static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
