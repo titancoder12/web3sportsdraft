b309a93663e3a8d67e25d4356b7db1827f187104
Eugene Lin <eugenelin89@gmail.com>
2025-03-13 13:22:32 -0700

    Added PlayerSerializer Added PlayerViewSet

--- api/views.py
+++ api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import *
+from league.models import *
+from .serializers import *
+from rest_framework import permissions
 
 # Create your views here.
+class PlayerViewSet(viewsets.ModelViewSet):
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
+    permission_classes = [permissions.IsAuthenticated]
