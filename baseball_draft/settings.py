8175005d30bd39266d7b91b5f6b0614638c16611
Eugene Lin <eugenelin89@gmail.com>
2025-04-02 15:19:03 -0700

    remove reading email verification setting from .env

--- baseball_draft/settings.py
+++ baseball_draft/settings.py
@@ -149,23 +149,23 @@ REST_FRAMEWORK = {
 
 # Mail Verification Settings
 # pip install django-environ
-import os
-import environ
-env = environ.Env()
+#import os
+#import environ
+#env = environ.Env()
 
-env_path = BASE_DIR / '.env'
-environ.Env.read_env(env_path)
+#env_path = BASE_DIR / '.env'
+#environ.Env.read_env(env_path)
 
-print("DEBUG — .env found?", os.path.exists(".env"))
-print("DEBUG — EMAIL_HOST_USER:", env("EMAIL_HOST_USER", default="NOT SET"))
+#print("DEBUG — .env found?", os.path.exists(".env"))
+#print("DEBUG — EMAIL_HOST_USER:", env("EMAIL_HOST_USER", default="NOT SET"))
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = env('EMAIL_HOST_USER')  # ← Your Gmail address
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')  # ← The 16-character App Password
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_HOST = 'smtp.gmail.com'
+#EMAIL_PORT = 587
+#EMAIL_USE_TLS = True
+##EMAIL_HOST_USER = env('EMAIL_HOST_USER')  # ← Your Gmail address
+#EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')  # ← The 16-character App Password
+#DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
 
